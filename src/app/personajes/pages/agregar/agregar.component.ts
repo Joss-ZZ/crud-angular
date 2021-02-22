d81358c89eb9daf1eb75d80a9daf086b4dfa86ab
Jhosimar <coderjzz@gmail.com>
2021-02-21 19:14:11 -0500

    CRUD finalizado

--- src/app/personajes/pages/agregar/agregar.component.ts
+++ src/app/personajes/pages/agregar/agregar.component.ts
@@ -5,6 +5,7 @@ import {switchMap} from 'rxjs/operators';
 
 import { PersonajesService } from '../../services/personajes.service';
 import { Personaje } from '../../interfaces/personaje.interface';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-agregar',
@@ -12,6 +13,8 @@ import { Personaje } from '../../interfaces/personaje.interface';
   styleUrls: ['./agregar.component.css']
 })
 export class AgregarComponent implements OnInit {
+
+  duracionSnackbar: number = 3;
 
   generos = [
     {
@@ -30,7 +33,8 @@ export class AgregarComponent implements OnInit {
 
   constructor(private activatedRoute: ActivatedRoute,
               private personajesService: PersonajesService,
-              private router: Router) { }
+              private router: Router,
+              private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
 
@@ -50,17 +54,29 @@ export class AgregarComponent implements OnInit {
     if(!this.personaje.id){ 
       // Registra
       this.personajesService.registrarPersonaje(this.personaje)
-        .subscribe(personaje => this.router.navigate(['/personajes/listado']));
+        .subscribe(personaje => {
+          this.mostrarSnackbar('Registro exitoso!'),
+          this.router.navigate(['/personajes/listado'])});
     }else{
       // Actualiza
       this.personajesService.editarPersonaje(this.personaje)
-        .subscribe(personaje => this.router.navigate(['/personajes/listado']));
+        .subscribe(personaje => {
+          this.mostrarSnackbar('Actualizado correctamente!'),
+          this.router.navigate(['/personajes/listado'])});
     }
   }
 
   eliminar(){
     this.personajesService.eliminarPersonaje(this.personaje!)
-      .subscribe(personaje => this.router.navigate(['/personajes/listado']));
+      .subscribe(personaje => {
+        this.mostrarSnackbar('Borrado correctamente!'),
+        this.router.navigate(['/personajes/listado'])});
+  }
+
+  mostrarSnackbar(mensaje: string){
+    this.snackBar.open(mensaje, 'Ok', {
+      duration: this.duracionSnackbar*1000
+    });
   }
 
 }
